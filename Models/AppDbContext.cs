10249830b9b3a18d97e378283c3b74d9de94d2e4
Vu Tien Hoa <hoavt184@gamil.com>
2021-10-10 16:04:31 +0700

    Created App

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Models{
+
+    public class AppDbContext : DbContext {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) {
+            base.OnConfiguring(builder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
